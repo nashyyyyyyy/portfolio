f6584229b48f831d28665ee474571dc8ad76d837
Nasar Saabdula <mrrious26@gmail.com>
2024-02-17 12:33:06 +0800

    updated infos

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ include 'server.php';
     <title>Portfolio</title>
     <link rel="shortcut icon" type="image/x-icon" href="img/nas.png">
     <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" >
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Audiowide"> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -31,13 +32,13 @@ include 'server.php';
             <a class="nav-link text-warning" href="#">HOME</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-light" href="#">ABOUT</a>
+            <a class="nav-link text-light" href="#about" id="myLink">ABOUT ME</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-light" href="#">CONTACT</a>
+            <a class="nav-link text-light" href="#contact">CONTACT</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-light" href="#">ACCOUNT</a>
+            <a class="nav-link text-light" href="#" >ACCOUNT</a>
           </li>
         </ul>
       </div>
@@ -60,9 +61,33 @@ include 'server.php';
     </div>
   </div>
 
-  <section class="">
-    <h1>HELLO</h1>
-  </section>
+  <section id="about" class="bg-dark-transparent">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="text-white text-center">ABOUT ME</h1>
+                <p class="text-white">I aspire to become a software engineer, crafting innovative solutions to complex problems and contributing to the advancement of technology. I'm passionate about learning new technologies and continuously improving my skills.</p>
+                <p class="text-white">One cool thing about me is, I am into the Minimalist lifestyle and it transfers to all of the things I do including this Portfolio, I am just amazed on how relaxing and pleasing minimalism is changing my perspective on everything.</p>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="contact" class="bg-dark-transparent">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 class="text-white text-center">CONTACT</h1>
+                    <div class="social-icons text-center">
+                        <a href="https://www.facebook.com/YourPage" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                        <a href="https://www.instagram.com/YourProfile" target="_blank"><i class="fab fa-instagram"></i></a>
+                        <a href="https://github.com/YourUsername" target="_blank"><i class="fab fa-github"></i></a>
+                        <a href="https://www.tiktok.com/@YourUsername" target="_blank"><i class="fab fa-tiktok"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
